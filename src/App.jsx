c9c52b424598e5d901039f95a90bfb3429694af8
Armen <armenshahbaz100@gmail.com>
2024-05-13 07:01:19 +0400

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
-import './App.scss'
+import  "./App.scss"
+
+
+
 
 export default function App() {
   return (
-    <div className='App container'>
-      <h1>Test</h1>
+    <div className='App'>
+      <div className='App_Desc'>
+      <img src="/images/1.png" alt="" />
+        <h1>Our values</h1>
+          <p>
+           Our story is one of continuous growth and evolution.We started as a small team
+           with big dreams, determined to create a real estate platform that transcended the 
+           ordinary.
+          </p>
+      </div>
+
+      <div className="App_content">
+          <div className="First_content">
+            <div className='name'>
+            <img src="/images/2.png" alt="" />
+            <h2>Trust</h2> 
+            </div>
+            <p>Trust is the cornestrone of 
+            every succesful real estate transaction
+            </p>
+            <div className="Second_content">
+            <div className="name">
+                <img src="/images/4.png" alt="" />
+                <h2>Client-Centric</h2>
+                </div>
+                <p>Your dreams and needs are at the center of our universe.We listen,understand </p>
+          </div>
+          </div>
+            
+          <div className="App_content_second">
+              <div className="name">
+                <img src="/images/3.png" alt="" />
+                <h2>Excellence</h2>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur rerum aperiam adipisci explicabo nemo libero.</p>
+      <div className="s">
+          <div className="name">
+            <img src="/images/2.png" alt="" />
+            <h2>Our Commitment</h2>
+          </div>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sequi assumenda aspernatur possimus maxime ratione!</p>
+           </div>
+          </div>
+          </div>
+
+         
+            
+           
+           
+      
     </div>
   )
 }
---
+++ src/features/apartmentsSlice.js
@@ -0,0 +1,45 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const initialState = [
+//   {
+//     id: 1,
+//     image: "/images/1.png",
+//     name: "Our values",
+//     desc: "Our story is one of continuous growth and evolution. We started as a small team with big dreams, determined to create a real estate platform that transcended the ordinary.",
+//   },
+//   {
+//     id: 2,
+//     image: "/images/2.png",
+//     name: "Trust",
+//     desc: "Trust is the cornestrone of every successful real estate transaction.",
+//   },
+//   {
+//     id: 3,
+//     image: "/images/3.png",
+//     name: "Excellence",
+//     desc: "We set the bar high for ourselves. From the properties we list to the services we provide.",
+//   },
+//   {
+//     id: 4,
+//     image: "/images/4.png",
+//     name: "Client-Centric",
+//     desc: "Your dreams and needs are at the center of our universe.We listen, understand.",
+//   },
+//   {
+//     id: 5,
+//     image: "/images/5.png",
+//     name: "Our Commitment",
+//     desc: "We are dedicated to providing you with the highest level of service, proffesionalism.",
+//   }, 
+// ]
+
+// const apartmentsSlice = createSlice({
+//   name: "apartments",
+//   initialState,
+//   selectors: {
+//     getApartments: (state) => state,
+//   }
+// })
+
+// export default apartmentsSlice.reducer;
+// export const { getApartments } = apartmentsSlice.selectors; 
